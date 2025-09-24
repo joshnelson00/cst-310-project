cf974971ca6648ac2489f6500c7fb69f5ff9d73f
joshnelson00 <joshuaknelson1202@icloud.com>
2025-09-24 10:02:41 -0700

    added severl elements

--- basic.cpp
+++ basic.cpp
@@ -98,8 +98,9 @@ int main()
 
     // --- Glasses Case (on top of Bible) ---
     std::vector<std::pair<int,int>> corners1 = {
-        {400, 590}, {480, 590}, {480, 610}, {400, 610}
+    {400, 528}, {480, 528}, {480, 560}, {400, 560}
     };
+
     glm::vec4 color1 = rgb255(105,105,107); // gray
     std::vector<GLfloat> vertices1 = createPrismVertices(corners1,-0.5f,-0.6f);
 
@@ -115,8 +116,9 @@ int main()
 
     // --- Bible ---
     std::vector<std::pair<int,int>> corners2 = {
-        {376,610}, {505,610}, {505,632}, {376,632}
+    {380, 560}, {505, 560}, {505, 582}, {380, 582}
     };
+
     glm::vec4 color2 = rgb255(150,153,149);// light gray
     std::vector<GLfloat> vertices2 = createPrismVertices(corners2,-0.5f,-0.8f);
 
@@ -155,10 +157,12 @@ int main()
 
     // --- DVD Player Pt1 ---
     std::vector<std::pair<int,int>> corners4 = {
-        {340,712}, {438,712}, {438,729}, {340,729}
+    {356, 662}, {454, 662}, {454, 695}, {356, 695}
     };
+
+
     glm::vec4 color4 = rgb255(43, 43, 41); // light gray
-    std::vector<GLfloat> vertices4 = createPrismVertices(corners4,-0.5f,-1.0f);
+    std::vector<GLfloat> vertices4 = createPrismVertices(corners4,-0.6f,-1.0f);
 
     GLuint VAO4,VBO4;
     glGenVertexArrays(1,&VAO4);
@@ -172,10 +176,11 @@ int main()
 
     // --- DVD Player Pt2 (flush with Pt1) ---
     std::vector<std::pair<int,int>> corners5 = {
-        {438,712}, {471,712}, {471,729}, {438,729}
+        {454, 662}, {487, 662}, {487, 695}, {454, 695}
     };
+
     glm::vec4 color5 = rgb255(10, 10, 10); // near black
-    std::vector<GLfloat> vertices5 = createPrismVertices(corners5,-0.5f,-1.0f);
+    std::vector<GLfloat> vertices5 = createPrismVertices(corners5,-0.6f,-1.0f);
 
     GLuint VAO5,VBO5;
     glGenVertexArrays(1,&VAO5);
@@ -189,8 +194,9 @@ int main()
 
     // Cabinet Top ---
     std::vector<std::pair<int,int>> corners6 = {
-        {0, 632}, {702,632}, {702,644}, {0,644}
+    {0, 582}, {702, 582}, {702, 594}, {0, 594}
     };
+
     glm::vec4 color6 = rgb255(70, 46, 29); // brown (will add texture)
     std::vector<GLfloat> vertices6 = createPrismVertices(corners6,-0.5f,-1.0f);
 
@@ -203,6 +209,96 @@ int main()
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(GLfloat),(GLvoid*)0);
     glEnableVertexAttribArray(0);
     glBindVertexArray(0);
+
+    // Cabinet Bottom Base ---
+    std::vector<std::pair<int,int>> corners7 = {
+        {0, 880}, {702,880}, {702,906}, {0,906}
+    };
+    glm::vec4 color7 = rgb255(27, 26, 24); // black base
+    std::vector<GLfloat> vertices7 = createPrismVertices(corners7,-0.5f,-1.0f);
+
+    GLuint VAO7,VBO7;
+    glGenVertexArrays(1,&VAO7);
+    glGenBuffers(1,&VBO7);
+    glBindVertexArray(VAO7);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO7);
+    glBufferData(GL_ARRAY_BUFFER,vertices7.size()*sizeof(GLfloat),vertices7.data(),GL_STATIC_DRAW);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(GLfloat),(GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    // Cabinet Base Support 1 ---
+    std::vector<std::pair<int,int>> corners8 = {
+    {201, 594}, {215, 594}, {215, 880}, {201, 880}
+    };
+
+    glm::vec4 color8 = rgb255(27, 26, 24); // black base
+    std::vector<GLfloat> vertices8 = createPrismVertices(corners8,-0.5f,-1.0f);
+
+    GLuint VAO8,VBO8;
+    glGenVertexArrays(1,&VAO8);
+    glGenBuffers(1,&VBO8);
+    glBindVertexArray(VAO8);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO8);
+    glBufferData(GL_ARRAY_BUFFER,vertices8.size()*sizeof(GLfloat),vertices8.data(),GL_STATIC_DRAW);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(GLfloat),(GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    // Cabinet Base Support 2 (mirrored on right side)
+    std::vector<std::pair<int,int>> corners9 = {
+        {487, 594}, {501, 594}, {501, 880}, {487, 880}
+    };
+
+    glm::vec4 color9 = rgb255(27, 26, 24); // same black base
+    std::vector<GLfloat> vertices9 = createPrismVertices(corners9,-0.5f,-1.0f);
+
+    GLuint VAO9,VBO9;
+    glGenVertexArrays(1,&VAO9);
+    glGenBuffers(1,&VBO9);
+    glBindVertexArray(VAO9);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO9);
+    glBufferData(GL_ARRAY_BUFFER,vertices9.size()*sizeof(GLfloat),vertices9.data(),GL_STATIC_DRAW);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(GLfloat),(GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    // Cabinet Back
+    std::vector<std::pair<int,int>> corners10 = {
+        {0, 594}, {702, 594}, {702, 906}, {0, 906}
+    };
+
+    glm::vec4 color10 = rgb255(6, 6, 6); // same black base
+    std::vector<GLfloat> vertices10 = createPrismVertices(corners10,-0.9f,-1.0f);
+
+    GLuint VAO10,VBO10;
+    glGenVertexArrays(1,&VAO10);
+    glGenBuffers(1,&VBO10);
+    glBindVertexArray(VAO10);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO10);
+    glBufferData(GL_ARRAY_BUFFER,vertices10.size()*sizeof(GLfloat),vertices10.data(),GL_STATIC_DRAW);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(GLfloat),(GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    // --- Shelf under DVD player ---
+    std::vector<std::pair<int,int>> corners11 = {
+        {201, 695}, {501, 695}, {501, 705}, {201, 705}
+    };
+
+    glm::vec4 color11 = rgb255(21, 18, 18); // dark shelf
+    std::vector<GLfloat> vertices11 = createPrismVertices(corners11, -0.5f, -1.0f);
+
+    GLuint VAO11, VBO11;
+    glGenVertexArrays(1, &VAO11);
+    glGenBuffers(1, &VBO11);
+    glBindVertexArray(VAO11);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO11);
+    glBufferData(GL_ARRAY_BUFFER, vertices11.size() * sizeof(GLfloat), vertices11.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
 
 
     // --- Render loop ---
@@ -278,6 +374,38 @@ int main()
         glBindVertexArray(VAO6);
         glDrawArrays(GL_TRIANGLES, 0, vertices6.size()/3);
         glBindVertexArray(0);
+
+        // Draw Cabinet Base
+        glUniform4fv(glGetUniformLocation(shader.Program, "prismColor"), 1, glm::value_ptr(color7));
+        glBindVertexArray(VAO7);
+        glDrawArrays(GL_TRIANGLES, 0, vertices7.size()/3);
+        glBindVertexArray(0);
+
+        // Draw Cabinet Base Support 1
+        glUniform4fv(glGetUniformLocation(shader.Program, "prismColor"), 1, glm::value_ptr(color8));
+        glBindVertexArray(VAO8);
+        glDrawArrays(GL_TRIANGLES, 0, vertices8.size()/3);
+        glBindVertexArray(0);
+
+        // Draw Cabinet Base Support 2
+        glUniform4fv(glGetUniformLocation(shader.Program, "prismColor"), 1, glm::value_ptr(color9));
+        glBindVertexArray(VAO9);
+        glDrawArrays(GL_TRIANGLES, 0, vertices9.size()/3);
+        glBindVertexArray(0);
+
+        // Draw Cabinet Base Support 2
+        glUniform4fv(glGetUniformLocation(shader.Program, "prismColor"), 1, glm::value_ptr(color10));
+        glBindVertexArray(VAO10);
+        glDrawArrays(GL_TRIANGLES, 0, vertices10.size()/3);
+        glBindVertexArray(0);
+
+        // Draw Shelf
+        glUniform4fv(glGetUniformLocation(shader.Program, "prismColor"), 1, glm::value_ptr(color11));
+        glBindVertexArray(VAO11);
+        glDrawArrays(GL_TRIANGLES, 0, vertices11.size()/3);
+        glBindVertexArray(0);
+
+
 
         glfwSwapBuffers(window);
     }
